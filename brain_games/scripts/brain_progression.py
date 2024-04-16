b0f69364abd7c53fac13fac7c95784e35892885a
ShchenevS <shchenevs@mail.ru>
2024-04-17 00:01:52 +0500

    rename progr.py to progression.py and fix script

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import brain_games.engine
-from brain_games.games import progr
+from brain_games.games import progression
 
 
 def main():
-    brain_games.engine.run_games(progr)
+    brain_games.engine.run_games(progression)
 
 
 if __name__ == '__main__':
